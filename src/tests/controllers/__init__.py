0f5ff6281087804593c96529b4b18b7377ea8bfa
azziimalek <malek.azzi@transformatek.dz>
2025-02-16 15:03:52 +0000

    Update test init.

--- src/tests/controllers/__init__.py
+++ src/tests/controllers/__init__.py
@@ -1,4 +1,4 @@
-from . import test_order_controllers
+from . import test_cart_controllers
 from . import test_service_controllers
 from . import test_cart_line_controllers
 from . import test_parameter_controller
