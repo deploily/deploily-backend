2c1b223547b31dfdd98c6546acb94d5c926f2227
azziimalek <malek.azzi@transformatek.dz>
2025-02-24 15:54:50 +0000

    fix warnings

--- src/app/models/parameters_value_models.py
+++ src/app/models/parameters_value_models.py
@@ -12,9 +12,9 @@ class ParameterValue(Model, AuditMixin):
     id = Column(Integer, primary_key=True)
     value = Column(String(255), nullable=False)
     parameter_id = Column(Integer, ForeignKey("parameter.id"))
-    parameter = relationship("Parameter", cascade="all,delete")
+    parameter = relationship("Parameter", cascade="all,delete" ,overlaps="parameters_values")
     cart_line_id = Column(Integer, ForeignKey("cart_line.id"))
-    cart_line = relationship("CartLine", cascade="all,delete")
+    cart_line = relationship("CartLine", cascade="all,delete",overlaps="parameters_values")
 
     def __repr__(self):
         return str(self.id)
