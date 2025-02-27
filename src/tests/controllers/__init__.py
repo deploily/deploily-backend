a508d9a3f1f942b1ffbb46cff852c16e6532b940
azziimalek <malek.azzi@transformatek.dz>
2025-02-27 15:02:03 +0000

    Add my favorite(test,model,view,controller).

--- src/tests/controllers/__init__.py
+++ src/tests/controllers/__init__.py
@@ -4,5 +4,6 @@ from . import (
     test_parameter_controller,
     test_service_controllers,
     test_support_ticket_controllers,
-    test_support_ticket_response_controllers
+    test_support_ticket_response_controllers,
+    test_my_favorite_controllers,
 )
