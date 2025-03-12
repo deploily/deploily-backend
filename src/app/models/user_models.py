edb5bc75e0e85b9c609af3d0d0fbf4cf6077900d
azziimalek <malek.azzi@transformatek.dz>
2025-03-12 14:04:50 +0000

    Add credits to user model .

--- src/app/models/user_models.py
+++ src/app/models/user_models.py
@@ -1,14 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from flask_appbuilder.security.sqla.models import User
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float
 from sqlalchemy.orm import relationship
 from flask_appbuilder import Model
 from flask_appbuilder.models.mixins import AuditMixin
 from flask import Markup, url_for
+from datetime import datetime, timedelta
+from app.models.my_service_models import MyService
+from app import appbuilder, db
 
 
 class MyUser(User):
     __tablename__ = "ab_user"
 
+    credits = Column(Float, default=0)
     phone = Column(String)
+    subscriptions = relationship(
+        "MyService", backref="user", foreign_keys="MyService.user_id")
+
+    @property
+    def existing_subscription(self):
+        """Retourne True si l'utilisateur a un abonnement actif"""
+        latest_subscription = (
+            db.session.query(MyService)
+            .filter_by(user_id=self.id)
+            .order_by(MyService.start_date.desc())
+            .first()
+        )
+
+        if not latest_subscription:
+            return False
+
+        expiration_date = latest_subscription.start_date.replace(day=1) + timedelta(
+            days=latest_subscription.duration_month * 30
+        )
+
+        return expiration_date > datetime.now().date()
