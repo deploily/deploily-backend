78e9746163566e4fc210b01d846e0d2a1dd80359
azziimalek <malek.azzi@transformatek.dz>
2025-03-18 13:44:50 +0000

    Add fields to profile view .

--- src/app/views/profile_views.py
+++ src/app/views/profile_views.py
@@ -10,9 +10,9 @@ from app.models.profile_models import Profile
 class ProfileModelView(ModelView):
     route_base = "/admin/profile"
     datamodel = SQLAInterface(Profile)
-    list_columns = ["id", "name", "type", "user_id"]
+    list_columns = ["id", "name", "type", "user_id", "phone",
+                    "company_name", "company_registration_number"]
     base_order = ("id", "desc")
-
 
 db.create_all()
 appbuilder.add_view(
