1f5a5ef22c144c3e04a1a4b6e5327ad639c5830a
azziimalek <malek.azzi@transformatek.dz>
2025-03-23 12:29:25 +0000

    Update init view .

--- src/app/views/__init__.py
+++ src/app/views/__init__.py
@@ -1,22 +1,22 @@
 from app import appbuilder, db
 
 from . import (
-    parameters_value_views,
-    service_parameters_views,
-    service_views,
-    subscribe_views,
-    support_ticket_views,
-    support_ticket_response_views,
-    user_views,
-    my_favorites_views,
     contact_us,
-    service_tag_views,
-    service_plan_views,
+    my_favorites_views,
+    parameters_value_views,
+    payment_views,
     plan_views,
-    service_plan_option_views,
     profile_views,
     promo_code_views,
-    payment_views
+    service_parameters_views,
+    service_plan_option_views,
+    service_plan_views,
+    service_tag_views,
+    service_views,
+    subscribe_views,
+    support_ticket_response_views,
+    support_ticket_views,
+    user_views,
 )
 
 appbuilder.add_link(
