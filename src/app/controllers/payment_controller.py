fe1fc55905a50e7345b12a0f8d57b8e7e5594d50
azziimalek <malek.azzi@transformatek.dz>
2025-03-23 12:22:09 +0000

    Add payment controller .

---
+++ src/app/controllers/payment_controller.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from flask_appbuilder.api import ModelRestApi
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+
+from app import appbuilder
+from app.models.payment_models import Payment
+
+_logger = logging.getLogger(__name__)
+_payment_display_columns = [
+    "id",
+    "amount",
+    "status",
+    "start_date",
+    "payment_method",
+    "subscription_id",
+    "profile_id",
+    "subscription",
+    "profile",
+]
+
+
+class PaymentModelApi(ModelRestApi):
+    resource_name = "payment"
+    base_order = ("id", "desc")
+    datamodel = SQLAInterface(Payment)
+    add_columns = _payment_display_columns
+    list_columns = _payment_display_columns
+    edit_columns = _payment_display_columns
+
+
+appbuilder.add_api(PaymentModelApi)
