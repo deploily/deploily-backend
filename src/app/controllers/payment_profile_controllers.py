0baea6031ed72d9d774b3ce074dc58c73bc5fc28
aichouche AMIR <aichouche.amir@transformatek.dz>
2025-04-14 11:04:14 +0000

    Add base filter for created_by field in PaymentProfileModelApi

--- src/app/controllers/payment_profile_controllers.py
+++ src/app/controllers/payment_profile_controllers.py
@@ -3,10 +3,12 @@
 import logging
 
 from flask_appbuilder.api import ModelRestApi
+from flask_appbuilder.models.sqla.filters import FilterEqualFunction
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 
 from app import appbuilder
 from app.models.payment_profile_models import PaymentProfile
+from app.utils.utils import get_user
 
 _logger = logging.getLogger(__name__)
 
@@ -36,6 +38,7 @@ class PaymentProfileModelApi(ModelRestApi):
     resource_name = "payment-profile"
     base_order = ("id", "desc")
     datamodel = SQLAInterface(PaymentProfile)
+    base_filters = [["created_by", FilterEqualFunction, get_user]]
     add_columns = _profile_display_columns
     list_columns = _profile_display_columns
     show_columns = _profile_display_columns
