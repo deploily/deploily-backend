6c163aade21f4b75af82c6b18c2108ff77760071
Boushaba Saadia <saadia.boushaba@transformatek.dz>
2025-04-28 13:51:49 +0000

    remove change_on created_on from views

--- src/app/views/contact_us.py
+++ src/app/views/contact_us.py
@@ -12,6 +12,9 @@ class ContactUsModelView(ModelView):
     datamodel = SQLAInterface(ContactUs)
     list_columns = ["id", "name", "email", "message"]
     base_order = ("id", "desc")
+    _exclude_columns = ["created_on", "changed_on"]
+    add_exclude_columns = _exclude_columns
+    edit_exclude_columns = _exclude_columns
 
 
 db.create_all()
