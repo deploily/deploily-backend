4d014e9afb11d6d66fb400396e851c38faa61b08
aichouche AMIR <aichouche.amir@transformatek.dz>
2025-06-02 12:55:52 +0000

    Add new endpoint to get all API services with detailed information

--- src/app/service_api/controllers/api_services_controller.py
+++ src/app/service_api/controllers/api_services_controller.py
@@ -3,7 +3,7 @@
 from flask_appbuilder.api import expose
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 
-from app import appbuilder
+from app import appbuilder, db
 from app.core.controllers.service_controllers import ServiceModelApi
 from app.service_api.models.api_services_model import ApiService
 
@@ -23,9 +23,75 @@ class ApiServiceModelApi(ServiceModelApi):
     show_columns = ServiceModelApi.show_columns + api_columns
     edit_columns = ServiceModelApi.edit_columns + api_columns
 
-    @expose("/custom_endpoint", methods=["GET"])
-    def custom_endpoint(self):
-        return self.response(200, message="Custom API for ApiService")
+    @expose("/all", methods=["GET"])
+    def get_all_api_services(self):
+        """
+        ---
+        get:
+          summary: Get all API services (base fields + service_plans + medias)
+          description: Returns a list of ApiService with name, short_description, description, service_plans and medias
+          responses:
+            200:
+              description: List of ApiServices
+              content:
+                application/json:
+                  schema:
+                    type: array
+                    items:
+                      type: object
+                      properties:
+                        name:
+                          type: string
+                        short_description:
+                          type: string
+                        description:
+                          type: string
+                        service_plans:
+                          type: array
+                          items:
+                            type: object
+                            additionalProperties: true
+                        medias:
+                          type: array
+                          items:
+                            type: object
+                            additionalProperties: true
+            500:
+              description: Internal server error
+        """
+        services = db.session.query(ApiService).all()
+
+        def serialize_service(service):
+            return {
+                "name": service.name,
+                "short_description": service.short_description,
+                "description": service.description,
+                "image_service": service.image_service,
+                "specifications": service.specifications,
+                "service_plans": [
+                    {
+                        "id": plan.id,
+                        "name": plan.plan.name,
+                        "price": plan.price,
+                    }
+                    for plan in service.service_plans
+                ],
+                "medias": [
+                    {
+                        "id": media.id,
+                        "name": media.title,
+                        "image": media.image,
+                    }
+                    for media in service.medias
+                ],
+            }
+
+        result = [serialize_service(s) for s in services]
+        return self.response(200, result=result)
+
+    # @expose("/custom_endpoint", methods=["GET"])
+    # def custom_endpoint(self):
+    #     return self.response(200, message="Custom API for ApiService")
 
 
 appbuilder.add_api(ApiServiceModelApi)
