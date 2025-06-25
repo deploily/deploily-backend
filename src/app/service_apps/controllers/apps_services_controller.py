a86c6429348e886ee2f6873a60c89c173259fc40
Boushaba Saadia <saadia.boushaba@transformatek.dz>
2025-06-25 14:52:07 +0000

    delete unnecessary fields

--- src/app/service_apps/controllers/apps_services_controller.py
+++ src/app/service_apps/controllers/apps_services_controller.py
@@ -10,16 +10,12 @@ from app.core.controllers.service_controllers import ServiceModelApi
 from app.service_apps.models.apps_services_model import AppService
 
 api_columns = [
-    "ssh_access",
-    "monitoring",
     "average_rating",
     "recommended_apps",
-    "app_slug",
     "min_apps_price",
     "minimal_cpu",
     "minimal_ram",
     "minimal_disk",
-
 ]
 
 
@@ -97,8 +93,6 @@ class AppServiceModelApi(ServiceModelApi):
                 "description": service.description,
                 "image_service": service.image_service,
                 "specifications": service.specifications,
-                "ssh_access": service.ssh_access,
-                "monitoring": service.monitoring,
                 "min_apps_price": service.min_apps_price,
                 "minimal_cpu": service.minimal_cpu,
                 "minimal_ram": service.minimal_ram,
