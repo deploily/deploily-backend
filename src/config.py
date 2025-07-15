329b8bdd640886795a9fc1e9be8668d03d82a895
benadda fatima <fatima.benadda@transformatek.dz>
2025-07-15 10:21:04 +0000

    Fix typo in permission for StatusApi to send PDF receipt mail

--- src/config.py
+++ src/config.py
@@ -235,7 +235,7 @@ FAB_ROLES = {
         ["TtkEpaySubscriptionApi", "can_upgrade_app_subscription"],
         ["SubscriptionApi", "can_upgrade_api_subscription"],
         ["SubscriptionApi", "can_renew_api_subscription"],
-        ["StatusApi", "can_senf_pdf_receipt_mail"],
+        ["StatusApi", "can_send_pdf_receipt_mail"],
     ]
 }
 
