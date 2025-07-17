78e9e1365fbaf82e4268544cd80883c44a10d460
benadda fatima <fatima.benadda@transformatek.dz>
2025-07-17 11:05:46 +0000

    Add check for expired subscriptions in API consumer creation

--- src/app/service_api/controllers/api_service_subscription_controller.py
+++ src/app/service_api/controllers/api_service_subscription_controller.py
@@ -75,6 +75,8 @@ class ApiServiceSubscriptionModelApi(SubscriptionModelApi):
 
         if not subscribe or not subscribe.service_plan:
             return Response("Subscription or ServicePlan not found", status=400)
+        if subscribe.is_expired:
+            return Response("Subscription is expired please renew your subscrption", status=400)
 
         base_service = subscribe.service_plan.service
 
