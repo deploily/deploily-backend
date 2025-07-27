a6b669459fef27946b831e00916e105b96205c22
benadda fatima <fatima.benadda@transformatek.dz>
2025-07-27 08:57:18 +0000

    add impor

--- src/app/service_apps/controllers/__init__.py
+++ src/app/service_apps/controllers/__init__.py
@@ -5,5 +5,6 @@ from . import subscription_app_service_controller
 from . import ttk_epay_subscribe_service_plan_controller
 from . import ttk_epay_subscription_controller
 from . import odoo_subscription_controller
+from . import supabase_subscription_controller
 from . import odoo_subscribe_service_plan_controller
 from . import supabase_subscribe_service_plan_controller
