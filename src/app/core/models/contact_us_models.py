1a82876365ddb1a8b22bbf42aeb6da724f67169f
benadda fatima <fatima.benadda@transformatek.dz>
2025-09-18 09:08:43 +0000

    Restore partner_id and partner relationship in ContactUs model

--- src/app/core/models/contact_us_models.py
+++ src/app/core/models/contact_us_models.py
@@ -13,8 +13,8 @@ class ContactUs(Model):
     phone = Column(String(20), nullable=True)
     ressource_id = Column(Integer, ForeignKey("ressource_service.id"))
     ressource = relationship("RessourceService", back_populates="contact_us")
-    # partner_id = Column(Integer, ForeignKey("ab_user.id"), nullable=True)
-    # partner = relationship("User", backref="contact_us")
+    partner_id = Column(Integer, ForeignKey("ab_user.id"), nullable=True)
+    partner = relationship("User", backref="contact_us")
 
     def __repr__(self):
         return self.name
