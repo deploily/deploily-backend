5a4fea0019b9b2c726b7c3066e57eeac861fdb19
Boushaba Saadia <saadia.boushaba@transformatek.dz>
2025-09-24 15:31:28 +0000

    add docker subscription controller

--- src/app/service_deployment/controllers/__init__.py
+++ src/app/service_deployment/controllers/__init__.py
@@ -1,4 +1,4 @@
 from . import deployment_services_controller
 from . import custom_parameter_controller
-from . import docker_deployment_subscription_controller
 from . import subscription_deployment_service_controller
+from . import docker_deployment_subscription_controller
