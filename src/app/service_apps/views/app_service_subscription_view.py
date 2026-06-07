7bb9445c91f7bdaec8621bbe82926f44a899f6af
benadda fatima <fatima.benadda@transformatek.dz>
2026-06-07 13:46:24 +0000

    Add TrialSubscriptionModelView to manage trial subscriptions

--- src/app/service_apps/views/app_service_subscription_view.py
+++ src/app/service_apps/views/app_service_subscription_view.py
@@ -2,6 +2,7 @@
 
 
 from flask_appbuilder import ModelView
+from flask_appbuilder.models.sqla.filters import FilterEqual
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 
 from app import appbuilder
@@ -37,5 +38,32 @@ appbuilder.add_view(
     SubscriptionAppServiceView,
     "Application Services Subscriptions",
     icon="fa-shopping-bag",
+    category="Subscriptions",
+)
+
+
+class TrialSubscriptionModelView(ModelView):
+    route_base = "/admin/trial-subscription"
+    datamodel = SQLAInterface(SubscriptionAppService)
+    list_columns = [
+        "id",
+        "created_by",
+        "type",
+        "byor",
+        "start_date",
+        "end_date",
+        "total_amount",
+    ]
+    base_order = ("start_date", "desc")
+    _exclude_columns = ["created_on", "changed_on"]
+    add_exclude_columns = _exclude_columns
+    edit_exclude_columns = _exclude_columns
+    base_filters = [["is_trial", FilterEqual, True]]
+
+
+appbuilder.add_view(
+    TrialSubscriptionModelView,
+    "Trial Subscriptions",
+    icon="fa-solid fa-shopping-basket",
     category="Subscriptions",
 )
